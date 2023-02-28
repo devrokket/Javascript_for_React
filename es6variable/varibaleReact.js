de7e384e577363d32c37017ee90fcc18306abd41
Byeong Rok <rokany@cau.ac.kr>
2023-03-01 01:56:06 +0900

    es6var

---
+++ es6variable/varibaleReact.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+
+class App extends Component {
+    render() {
+        const greeting = 'welcome React';
+        return (
+            <h1>greeting</h1>
+        )
+    }
+}
